285311c88ff9ebd57093bf4617d292e661255083
michaelhugi <michael.hugi@informaticon.com>
2022-04-08 10:44:51 +0200

    Finished tests and documentation

--- musical_note_just_test.go
+++ musical_note_just_test.go
@@ -1,13 +1,26 @@
 package go_hugipipes_musical_notes
 
-import "testing"
+import (
+	tu "github.com/informaticon/lib.go.base.test-utils"
+	"testing"
+)
 
 func TestMNoteEqual(t *testing.T) {
-	//tj := NewMTemperamentJust(C, A, Octave4, 440)
-	//o5 := tj.Octave(5)
-	//dis5 := o5.Note(Dis)
-
-	//ExactFrequency
-	//FrequencyBelongsToNote
-	//LowerFrequency
+	tj := NewMTemperamentJust(C, A, Octave4, 440)
+	o3 := tj.Octave(3)
+	dis3 := o3.Note(Dis)
+	tu.AssertEq(roundFloat(dis3.LowerFrequency(), 2), 152.51, t)
+	tu.AssertEq(roundFloat(dis3.ExactFrequency(), 2), 156.98, t)
+	tu.AssertEq(roundFloat(dis3.UpperFrequency(), 2), 161.58, t)
+	tu.Assert(!dis3.FrequencyBelongsToNote(152.50), t)
+	tu.Assert(dis3.FrequencyBelongsToNote(152.51), t)
+	tu.Assert(dis3.FrequencyBelongsToNote(161.57), t)
+	tu.Assert(!dis3.FrequencyBelongsToNote(161.58), t)
+	tu.AssertEq(dis3.MidiNoteNumber(), 51, t)
+	tu.AssertEq(dis3.Octave().Octave(), Octave3, t)
+	tu.AssertEq(roundFloat(dis3.OffsetInCents(152.51), 0), -50, t)
+	tu.AssertEq(roundFloat(dis3.OffsetInCents(156.98), 0), 0, t)
+	tu.AssertEq(roundFloat(dis3.OffsetInCents(161.58), 0), 50, t)
+	tu.AssertEq(dis3.String(), "D#3", t)
+	tu.AssertEq(dis3.Note(), Dis, t)
 }
